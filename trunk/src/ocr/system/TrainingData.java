efc1d16cf8037e304fa4bf1fee7b09195ff86689
devo149 <devo149@e66bd791-db32-f017-1045-c45753d9eb75>
2011-11-12 22:01:48 +0000

    Made TrainingData a singleton.

--- trunk/src/ocr/system/TrainingData.java
+++ trunk/src/ocr/system/TrainingData.java
@@ -1,6 +1,7 @@
 package ocr.system;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Collection;
 
 /**
@@ -17,13 +18,36 @@ public class TrainingData
    private Collection<CharacterFeaturePair> mData;
 
    /**
+    * Variable to hold a singleton of TrainingData
+    */
+   private static final TrainingData cInstance;
+
+   /**
+    * Initialize a Server instance
+    */
+   static
+   {
+      cInstance = new TrainingData();
+   }
+
+   private TrainingData()
+   {
+      mData = new ArrayList<CharacterFeaturePair>();
+   }
+
+   public TrainingData getInstance()
+   {
+      return cInstance;
+   }
+
+   /**
     * Insert a new character-feature pair into the training data
     *
     * @param pData The character-feature pair to store
     */
    public void insert(CharacterFeaturePair pData)
    {
-
+      mData.add(pData);
    }
 
    /**
