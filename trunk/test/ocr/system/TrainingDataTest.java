f34ceb6085950fdc18780dcfa748c3734da2bfea
devo149 <devo149@e66bd791-db32-f017-1045-c45753d9eb75>
2011-12-03 03:13:43 +0000

    Included more unit tests.

--- trunk/test/ocr/system/TrainingDataTest.java
+++ trunk/test/ocr/system/TrainingDataTest.java
@@ -1,5 +1,10 @@
 package ocr.system;
 
+import java.util.ArrayList;
+import junit.framework.Assert;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.ObjectInputStream;
 import java.util.Collection;
 import org.junit.After;
 import org.junit.AfterClass;
@@ -48,8 +53,65 @@ public class TrainingDataTest
    {
       System.out.println("getInstance");
       TrainingData expResult = null;
+
+      try
+      {
+         File file = new File(TrainingData.cFileName);
+         if (file.exists())
+         {
+            FileInputStream fis = new FileInputStream(file);
+            ObjectInputStream ois = new ObjectInputStream(fis);
+            expResult = (TrainingData) ois.readObject();
+            ois.close();
+         }
+      }
+      catch (Exception e)
+      {
+         e.printStackTrace();
+      }
+
+      if (expResult == null)
+      {
+         expResult = TrainingData.getInstance();
+      }
+
       TrainingData result = TrainingData.getInstance();
-      assertEquals(expResult, result);
+      isEqual(expResult, result);
+   }
+
+   /**
+    * Determine if two TrainingData objects are equal. They are considered
+    * equal if they contain the same data values.
+    * 
+    * @param expResult The expected training data
+    * @param result The training data to compare with
+    * @return True if the training data values are the same
+    */
+   private boolean isEqual(TrainingData expResult, TrainingData result)
+   {
+      boolean isEqual = true;
+
+      int i = 0;
+      ArrayList<CharacterFeaturePair> rData;
+      rData = (ArrayList<CharacterFeaturePair>)result.getData();
+      for (CharacterFeaturePair exp : expResult.getData())
+      {
+         CharacterFeaturePair res = rData.get(i);
+         if ((!exp.getCharacter().equals(res.getCharacter())) ||
+            (exp.getFeaturePoint() != exp.getFeaturePoint()))
+         {
+            isEqual = false;
+            break;
+         }
+         i++;
+      }
+
+      if (!isEqual)
+      {
+         Assert.failNotEquals("Training data does not match.", expResult, result);
+      }
+
+      return isEqual;
    }
 
    /**
@@ -73,7 +135,7 @@ public class TrainingDataTest
    public void testGetData()
    {
       System.out.println("getData");
-      TrainingData instance = null;
+      TrainingData instance = TrainingData.getInstance();
       Collection expResult = null;
       Collection result = instance.getData();
       assertEquals(expResult, result);
